8ac1baca25bd46932558a856de1f5b6a2cca98e5
yzy <1789263632@qq.com>
2026-01-04 00:52:54 +0800

    auto push 2026-01-04 00:52:54

--- configs/textrecog/svtr/svtr-large_fudan_scene_baseline.py
+++ configs/textrecog/svtr/svtr-large_fudan_scene_baseline.py
@@ -69,19 +69,22 @@ param_scheduler = [
 ]
 
 train_dataloader = deepcopy(_base_.fudan_scene_train_dataloader)
-train_dataloader['batch_size'] = 32
-train_dataloader['num_workers'] = 0
-train_dataloader['persistent_workers'] = False
+train_dataloader['batch_size'] = 128
+train_dataloader['num_workers'] = 2
+train_dataloader['prefetch_factor'] = 1
+train_dataloader['persistent_workers'] = True
 train_dataloader['dataset']['pipeline'] = train_pipeline
 
 val_dataloader = deepcopy(_base_.fudan_scene_val_dataloader)
-val_dataloader['num_workers'] = 0
-val_dataloader['persistent_workers'] = False
+val_dataloader['num_workers'] = 2
+val_dataloader['prefetch_factor'] = 1
+val_dataloader['persistent_workers'] = True
 val_dataloader['dataset']['pipeline'] = test_pipeline
 
 test_dataloader = deepcopy(_base_.fudan_scene_test_dataloader)
-test_dataloader['num_workers'] = 0
-test_dataloader['persistent_workers'] = False
+test_dataloader['num_workers'] = 2
+test_dataloader['prefetch_factor'] = 1
+test_dataloader['persistent_workers'] = True
 test_dataloader['dataset']['pipeline'] = test_pipeline
 
 val_evaluator = dict(
@@ -105,4 +108,4 @@ default_hooks = dict(
     ),
 )
 
-auto_scale_lr = dict(base_batch_size=64)
+auto_scale_lr = dict(base_batch_size=train_dataloader['batch_size'] * 2)
