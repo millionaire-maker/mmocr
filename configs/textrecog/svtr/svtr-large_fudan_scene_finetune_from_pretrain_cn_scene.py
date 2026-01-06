571521a44569b69d1040d0af7b55c829790d43f4
yzy <1789263632@qq.com>
2026-01-07 01:35:45 +0800

    auto push 2026-01-07 01:35:45

--- configs/textrecog/svtr/svtr-large_fudan_scene_finetune_from_pretrain_cn_scene.py
+++ configs/textrecog/svtr/svtr-large_fudan_scene_finetune_from_pretrain_cn_scene.py
@@ -1,5 +1,5 @@
 _base_ = ['svtr-large_fudan_scene_baseline.py']
 
-pretrain_work_dir = 'work_dirs/svtr-large_pretrain_cn_scene'
+pretrain_work_dir = 'work_dirs/svtr-large_pretrain'
 pretrain_epoch = 20
-load_from = f'{pretrain_work_dir}/epoch_{pretrain_epoch}.pth'
+load_from = f'{pretrain_work_dir}/best_PretrainCN_holdout_recog_word_acc_epoch_{pretrain_epoch}.pth'
