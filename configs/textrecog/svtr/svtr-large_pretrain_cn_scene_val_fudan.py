ae9107eb38402eca7e335999012a385940752fe6
yzy <1789263632@qq.com>
2026-01-07 13:43:28 +0800

    auto push 2026-01-07 13:43:28

---
+++ configs/textrecog/svtr/svtr-large_pretrain_cn_scene_val_fudan.py
@@ -0,0 +1,26 @@
+from copy import deepcopy
+
+_base_ = ['svtr-large_pretrain_cn_scene.py']
+
+# Validate directly on real Fudan val during synthetic pretraining so that
+# the reported scores reflect real-domain transfer instead of synthetic hold-out.
+fudan_val = dict(
+    type='RecogLMDBDataset',
+    data_root='data/fudan/scene',
+    ann_file='scene_val',
+    pipeline=deepcopy(_base_.test_pipeline),
+    test_mode=True,
+)
+
+val_dataloader = deepcopy(_base_.val_dataloader)
+val_dataloader['dataset'] = fudan_val
+
+test_dataloader = val_dataloader
+
+val_evaluator = deepcopy(_base_.val_evaluator)
+val_evaluator['dataset_prefixes'] = ['Fudan']
+test_evaluator = val_evaluator
+
+default_hooks = deepcopy(_base_.default_hooks)
+default_hooks['checkpoint']['save_best'] = 'Fudan/recog/word_acc'
+
