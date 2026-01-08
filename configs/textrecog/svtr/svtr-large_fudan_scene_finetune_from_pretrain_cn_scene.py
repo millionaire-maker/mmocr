e0b8d763dcef9d48c3321083bec0a944a0b93047
yzy <1789263632@qq.com>
2026-01-08 18:30:33 +0800

    auto push 2026-01-08 18:30:32

--- configs/textrecog/svtr/svtr-large_fudan_scene_finetune_from_pretrain_cn_scene.py
+++ configs/textrecog/svtr/svtr-large_fudan_scene_finetune_from_pretrain_cn_scene.py
@@ -1,5 +1,5 @@
 _base_ = ['svtr-large_fudan_scene_baseline.py']
 
 pretrain_work_dir = 'work_dirs/svtr-large_new_pretrain'
-pretrain_epoch = 13
-load_from = f'{pretrain_work_dir}/best_Fudan_recog_word_acc_epoch_{pretrain_epoch}.pth'
+pretrain_epoch = 20
+load_from = f'{pretrain_work_dir}/epoch_{pretrain_epoch}.pth'
