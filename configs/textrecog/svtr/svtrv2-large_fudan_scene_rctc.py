dd87fdc0785fabd7ad0b226403717afa3cdc82c7
yzy <1789263632@qq.com>
2026-01-11 11:56:52 +0800

    auto push 2026-01-11 11:56:52

--- configs/textrecog/svtr/svtrv2-large_fudan_scene_rctc.py
+++ configs/textrecog/svtr/svtrv2-large_fudan_scene_rctc.py
@@ -3,5 +3,5 @@ _base_ = ['svtrv2-large_fudan_scene_baseline.py']
 # SVTRv2 stage-1 style ablation: MSR + FRM, without SGM.
 # This mirrors OpenOCR's first-stage training recipe (`svtrv2_rctc.yml`),
 # where SGM is introduced in the second stage.
+use_sgm = False
 model = dict(decoder=dict(sgm=dict(enabled=False)))
-
--- configs/textrecog/svtr/svtrv2-large_pretrain_cn_scene_plus_gapfix_val_fudan_rctc.py
+++ configs/textrecog/svtr/svtrv2-large_pretrain_cn_scene_plus_gapfix_val_fudan_rctc.py
@@ -1,5 +1,5 @@
 _base_ = ['svtrv2-large_pretrain_cn_scene_plus_gapfix_val_fudan.py']
 
 # SVTRv2 stage-1 pretraining ablation: MSR + FRM, without SGM.
+use_sgm = False
 model = dict(decoder=dict(sgm=dict(enabled=False)))
-
