671ff2e8cf45954b81c9533a6fd20c5c585ab793
yzy <1789263632@qq.com>
2026-01-13 12:54:49 +0800

    auto push 2026-01-13 12:54:49

--- configs/textrecog/svtr/.ipynb_checkpoints/svtr-large_fudan_scene_finetune_from_pretrain_cn_scene_tpspp-checkpoint.py
+++ configs/textrecog/svtr/.ipynb_checkpoints/svtr-large_fudan_scene_finetune_from_pretrain_cn_scene_tpspp-checkpoint.py
@@ -1,7 +1,7 @@
 _base_ = ['svtr-large_fudan_scene_finetune_from_pretrain_cn_scene.py']
 
 pretrain_work_dir = 'work_dirs/svtr-large_pretrain_cn_scene_tpspp'
-pretrain_epoch = 19
+pretrain_epoch = 20
 load_from = f'{pretrain_work_dir}/epoch_{pretrain_epoch}.pth'
 
 model = dict(
